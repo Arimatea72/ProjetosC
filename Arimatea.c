8fbd4c6ed812c3f956ece4b752c831527372395f
Arimatéa Sousa <arimatea.sousa@gmail.com>
2025-08-02 12:58:41 +0000

    atualização kate.c

---
+++ Arimatea.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Formado em administação, faculdade FA7!\n");
+  return 0;
+}
--- Kate.c
+++ Kate.c
@@ -1,1 +1,6 @@
+#include <stdio.h>
 
+int main() {
+  printf("Kate tem aproximadamente três anos!\n");
+  return 0;
+}
